e693d67e119cace6bdf6d69b08465a75ad28efb3
marco-gulino <marcogulino98@gmail.com>
2025-02-26 15:56:56 +0100

    [feat] create route for PostPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// IMPORT CSS Files
 import "./App.css";
+// PAGES
+import Postpage from "./pages/PostPage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index Component={Postpage} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
